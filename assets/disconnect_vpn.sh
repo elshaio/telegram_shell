9a32b0a61ce071a228ff4c1f981ff16a9723c680
Iván Román <develdkmd@outlook.com>
2021-05-05 22:35:57 -0600

    ADD Way to send files through main script

--- assets/disconnect_vpn.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo pkill openvpn
-sleep 3
-for i in {1..50}
-do
-  sleep 1 && nc -w 2 -z api.telegram.org 443 && break
-done
-
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import logging
 from telegram.ext import Updater
+from telegram import Bot
 from telegram_utils import command_generator
 import config
 import sys
 
 logging.basicConfig(format='[%(asctime)s] %(levelname)s %(name)s - %(message)s',
-                    level=logging.INFO)
+                    level=logging.INFO, filename="logs")
 
 logger = logging.getLogger('main')
 
@@ -24,6 +25,16 @@ def main():
     logger.info('Bot iniciado')
 
 
+def enviar_mensaje(id, mensaje):
+    bot = Bot(config.telegram_token)
+    bot.send_message(id, ' '.join(mensaje))
+
+
+def enviar_archivo(id, archivo):
+    bot = Bot(config.telegram_token)
+    bot.send_document(chat_id=id, document=open(archivo[0], 'rb'))
+
+
 if __name__ == '__main__':
     args = sys.argv[1:]
 
@@ -33,6 +44,10 @@ if __name__ == '__main__':
 
     if option == 'commands':
         command_generator.print_commands()
+    elif option == 'mensaje':
+        enviar_mensaje(args[1], args[2:])
+    elif option == 'archivo':
+        enviar_archivo(args[1], args[2:])
     else:
         main()
 
