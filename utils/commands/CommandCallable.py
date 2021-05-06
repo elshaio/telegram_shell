462c1ba828d24ad851eab6a5fbc9c7e4bc3a8e60
Iván Román <develdkmd@outlook.com>
2021-05-05 22:28:42 -0600

    ADD Exclutions for groups and use of my shell package

--- utils/commands/CommandCallable.py
+++ utils/commands/CommandCallable.py
@@ -1,11 +1,11 @@
-from utils import shell
+import shell_pipes as shell
 import logging
 
 logger = logging.getLogger('CommandCallable')
 
 
 def _voidcallback():
-    return
+    return None
 
 
 class CommandCallable:
@@ -19,11 +19,37 @@ class CommandCallable:
         self._text = kwargs.get('text', 'done')
         self._no_callback = kwargs.get('no_callback', False)
         self._description = kwargs.get('description', '')
+        self._group_id = kwargs.get('group_id')
 
     def __call__(self, *args, **kwargs):
         logger.info('Ejecutando {}'.format(self._description))
-        response = shell.execute(self._command)
-        text = '\n'.join(response)
+
+        ejecutar = False
+
+        text = args[0]['message']['text'].split(' ', 1)
+        if len(text) == 1:
+            text = ''
+        else:
+            text = text[1]
+
+        chat = args[0]['message']['chat']
+        logger.info(chat['id'])
+
+        if self._group_id is not None:
+            chat = args[0]['message']['chat']
+            logger.info("chatmensaje = {}".format(chat))
+            logger.info(self._group_id)
+
+            if (chat['type'] == 'group' or chat['type'] == 'supergroup') and chat['id'] == self._group_id:
+                ejecutar = True
+        else:
+            ejecutar = True
+
+        if ejecutar:
+            response = shell.execute(self._command + ' ' + text)
+            text = '\n'.join(response)
+        else:
+            text = 'No se tienen permisos para ejecutar el comando'
 
         if self._use_response and text != '':
             to_response = text
@@ -39,6 +65,7 @@ class CommandCallable:
             kwargs = dict(command_response=to_response, **kwargs)
 
         if not self._no_callback:
+            # noinspection PyArgumentList
             self._callback(*args, **kwargs)
 
         if self._use_response:
